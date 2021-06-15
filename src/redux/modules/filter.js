7e66997bd67a6a63bba4bb7092c18902bb0cfc6b
Ahn,Gi-Hwan <ahngh0113@naver.com>
2021-06-15 15:18:32 +0900

    redux-actions

--- src/redux/modules/filter.js
+++ src/redux/modules/filter.js
@@ -1,30 +1,51 @@
-// action type define
-const SHOW_ALL = "react-redux/filter/SHOW_ALL";
-const SHOW_COMPLETE = "react-redux/filter/SHOW_COMPLETE";
+import { createActions, handleActions } from "redux-actions";
 
-// action create function
-export function showAll() {
-  return {
-    type: SHOW_ALL,
-  };
-}
-export function showComplete() {
-  return {
-    type: SHOW_COMPLETE,
-  };
-}
+export const { showAll, showComplete } = createActions(
+  "SHOW_ALL",
+  "SHOW_COMPLETE",
+  {
+    prefix: "react-redux/filter",
+  }
+);
+
+// // action type define
+// const SHOW_ALL = "react-redux/filter/SHOW_ALL";
+// const SHOW_COMPLETE = "react-redux/filter/SHOW_COMPLETE";
+
+// // action create function
+// export function showAll() {
+//   return {
+//     type: SHOW_ALL,
+//   };
+// }
+// export function showComplete() {
+//   return {
+//     type: SHOW_COMPLETE,
+//   };
+// }
 
 // initialValue
 const initialState = { filter: "ALL" };
 
-// reducer
-export default function reducer(previousState = initialState, action) {
-  if (action.type === SHOW_ALL) {
-    return "ALL";
-  }
-  if (action.type === SHOW_COMPLETE) {
-    return "COMPELTE";
-  }
+const reducer = handleActions(
+  {
+    SHOW_ALL: (state, action) => "ALL",
+    SHOW_COMPLETE: (state, action) => "COMPLETE",
+  },
+  initialState,
+  { prefix: "react-redux/filter" }
+);
 
-  return previousState;
-}
+// // reducer
+// export default function reducer(previousState = initialState, action) {
+//   if (action.type === SHOW_ALL) {
+//     return "ALL";
+//   }
+//   if (action.type === SHOW_COMPLETE) {
+//     return "COMPELTE";
+//   }
+
+//   return previousState;
+// }
+
+export default reducer;
